db001407be9a6ab9d6726b0ab93e45c688e12f33
abtin <abtinfiroozyar@gmail.com>
2024-08-06 09:29:21 +0330

    setup login and logout with user register

--- django_flashcard/home/urls.py
+++ django_flashcard/home/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path(
         "flashcard/<int:card_id>", views.ShowAnswerFlashcard.as_view(), name="show_card"
     ),
-    path("login/", views.UserLoginView.as_view(), name="user_login")
+    path("login/", views.UserLoginView.as_view(), name="user_login"),
+    path("register/", views.UserRegisterView.as_view(), name="user_register"),
+    path("logout/", views.UserLogoutViwe.as_view(), name="user_logout")
 ]
--- django_flashcard/home/views.py
+++ django_flashcard/home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
-from .forms import UserLogInForm
+from .forms import UserLogInForm, UserRegisterForm
 from django.views import View
 from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 
@@ -28,12 +30,38 @@ class UserLoginView(View):
             if user is not None:
                 login(request, user)
                 messages.success(request, "user login successfuly", "success")
-                if self.next:
-                    return redirect(self.next)
-                return redirect("home:home")
+                return redirect(self.next) if self.next else redirect("home:home")
             messages.error(request, "username or password is wrong", "warning")
         return render(request, self.template_name, {"form": form})
+class UserRegisterView(View):
+    form_class = UserRegisterForm
+    template_form = "home/register.html"
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("home:home")
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_form, {"form": form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = User.objects.create_user(
+                username=cd["username"], email=cd["email"], password=cd["password"]
+            )
+            messages.success(request, "you registered successfuly", "success")
+            return redirect("home:home")
+        return render(request, self.template_form, {"form": form})
+
+class UserLogoutViwe(LoginRequiredMixin,View):
+    def get(self, request):
+        logout(request)
+        messages.success(request, "user logout successfuly", "success")
+        return redirect("home:home")
 
 class HomeView(View):
     def get(self, request):
