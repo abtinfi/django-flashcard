80fb0e0186e8905fb964c5cb3acb7a182d16240a
abtin <abtinfiroozyar@gmail.com>
2024-08-06 13:14:56 +0330

    fix bug

--- django_flashcard/api/models.py
+++ django_flashcard/api/models.py
@@ -1,46 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
-
 class FlashCard(models.Model):
     question = models.TextField()
     real_answer = models.TextField()
-    possible_answers_json = models.TextField(default=None)  # Renamed field
+    possible_answers = models.JSONField(default=list)  # Default is an empty list
 
-    @property
-    def possible_answers(self):
-        import json
-        return json.loads(self.possible_answers_json)
-
-    @possible_answers.setter
-    def possible_answers(self, value):
-        import json
-        self.possible_answers_json = json.dumps(value)
+    def __str__(self):
+        return self.question
 
 
 class UserAnswer(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_answers")  # Renamed related_name
-    flashcard_data_json = models.TextField()  # Renamed field
-    answer_log_json = models.TextField()  # Renamed field
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_answers")
+    flashcard_data = models.JSONField(default=dict)  # Default is an empty dictionary
+    answer_log = models.JSONField(default=list)  # Default is an empty list
 
-    @property
-    def flashcard_data(self):
-        import json
-        return json.loads(self.flashcard_data_json)
-
-    @flashcard_data.setter
-    def flashcard_data(self, value):
-        import json
-        self.flashcard_data_json = json.dumps(value)
-
-    @property
-    def answer_log(self):
-        import json
-        return json.loads(self.answer_log_json)
-
-    @answer_log.setter
-    def answer_log(self, value):
-        import json
-        self.answer_log_json = json.dumps(value)
+    def __str__(self):
+        return f"UserAnswer({self.user}, {self.flashcard_data})"
