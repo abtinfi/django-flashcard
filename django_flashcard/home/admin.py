5fa463aa3e3f0ad99ae0dc97329e96ed32ea764d
abtin <abtinfiroozyar@gmail.com>
2024-08-17 11:13:41 +0330

    add search filter for user flashcard review data in admin panel

--- django_flashcard/home/admin.py
+++ django_flashcard/home/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from api import models
 # Register your models here.
 
+class UserFlashcardAdmin(admin.ModelAdmin):
+    search_fields = ['user__username']
+    list_filter = ['user__username']
+
 admin.site.register(models.FlashCard)
-admin.site.register(models.UserFlashCard)
+admin.site.register(models.UserFlashCard, UserFlashcardAdmin)
 
